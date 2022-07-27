2659c224abdb00669177a5fb911baa735a584b25
Flavio Rodriguez <ihaio95@gmail.com>
2022-07-26 21:34:06 -0300

    configurando socket y rutas

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = 3000 || process.env.PORT;
+const routes = require("./routes")
+const registerWebsocket = require("./socket")
 require("./db")();
 
+app.use(express.json())
+app.use(express.static(__dirname + "/public"))
+app.use(routes)
 
-
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`)
 })
+
+registerWebsocket(server)
+
+
+
