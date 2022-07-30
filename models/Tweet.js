4895627bdc893a7b189280196a877a318e1e8590
Flavio Rodriguez <ihaio95@gmail.com>
2022-07-30 17:14:06 -0300

    arreglo del modelo tweets

--- models/Tweet.js
+++ models/Tweet.js
@@ -2,20 +2,23 @@ const { Schema, model } = require("mongoose")
 
 const tweetSchema = new Schema(
     {
-        tweet: String,
-        user: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "User",
-            },
-        ]
-
+        text: {
+            type: String,
+            required: true,
+            max: 128,
+        },
+        author: {
+            ref: "User",
+            type: Schema.Types.ObjectId,
+        },
     },
+
     {
         timestamps: true,
     }
-
 );
+
+
 
 const Tweet = model("Tweet", tweetSchema);
 
